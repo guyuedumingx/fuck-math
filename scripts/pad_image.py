35d259ac14d60d6fcb60a94049b73983ff6d46b6
guyuedumingx <guyuedumingx@gmail.com>
2022-04-21 21:12:57 +0800

    update readme

--- scripts/pad_image.py
+++ scripts/pad_image.py
@@ -1,24 +1,29 @@
 from PIL import Image
 import os
 
+
 def pad_image(image, to_width):
+    """统一文件夹内图片的宽度，谨慎使用"""
     iw, ih = image.size
+    #用白色填充
     nimg = Image.new('RGB', (to_width, ih), (256,256,256))
     nimg.paste(image,(0,0)) 
     return nimg
 
-path = "E:\数学\多么痛的领悟\chapter1\database\images"
-imgs_path = [path+os.sep+ i for i in os.listdir(path)]
-
-images = [Image.open(i) for i in imgs_path]
-
 def get_max_width(images):
-    max_width, _ = images[0].size;
+    """获取图片最大宽度"""
+    max_width = 0
     for img in images:
-        width, _ = img.size;
+        width, _ = img.size
         if max_width < width:
             max_width = width
     return max_width
+
+
+path = "..\chapter1\database\images"
+imgs_path = [path+os.sep+ i for i in os.listdir(path)]
+
+images = [Image.open(i) for i in imgs_path]
 
 max_width = get_max_width(images)
 
